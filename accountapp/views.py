f8c65c0e5b03924c125ec655b5afaffa9dfdb62b
chogoal <goal961@naver.com>
2021-07-08 15:09:59 +0900

    210708 commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(requests):
-    return render(requests, 'accountapp/hello_world.html')
+    # return HttpResponse('Hello World!') # alt + enter 로 간단히 import
+    if requests.method == 'POST':
+        return render(requests, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+    else:
+        return render(requests, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
